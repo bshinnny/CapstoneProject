fb22ecc90f8575cfab21aa6b38e9b55781d4bee0
Brandon Shin <brandonjshin98@gmail.com>
2022-12-22 07:32:28 -0800

    Migration refactored for render.com.

--- migrations/versions/20221222_064924_.py
+++ migrations/versions/20221222_064924_.py
@@ -81,7 +81,7 @@ def upgrade():
     sa.PrimaryKeyConstraint('id')
     )
     # ### end Alembic commands ###
-     if environment == "production":
+    if environment == "production":
         op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
         op.execute(f"ALTER TABLE products SET SCHEMA {SCHEMA};")
         op.execute(f"ALTER TABLE cart_items SET SCHEMA {SCHEMA};")
