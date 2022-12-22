d743d719a8392abca69c1fbf8bbb2cd53e476f7f
Brandon Shin <brandonjshin98@gmail.com>
2022-12-22 07:19:21 -0800

    Seeding refactored for render.com.

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,10 +19,10 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_products()
-        undo_cart_items()
         undo_orders()
+        undo_cart_items()
+        undo_products()
+        undo_users()
     seed_users()
     seed_products()
     seed_cart_items()
@@ -33,8 +33,12 @@ def seed():
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
-    undo_users()
+    # undo_users()
     # Add other undo functions here.
-    undo_products()
-    undo_cart_items()
+    # undo_products()
+    # undo_cart_items()
+    # undo_orders()
     undo_orders()
+    undo_cart_items()
+    undo_products()
+    undo_users()
