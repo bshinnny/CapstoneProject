286c02436eb9b49c23b02e628342abf4b47d15aa
Brandon Shin <brandonjshin98@gmail.com>
2023-01-05 11:54:48 -0800

    Added seeder data and minor UI improvements.

--- react-app/src/components/Cart/UserCart/index.js
+++ react-app/src/components/Cart/UserCart/index.js
@@ -1,20 +1,27 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+// import ProductCard from '../../Products/ProductCard';
 import * as cartActions from '../../../store/cart';
 import * as ordersActions from '../../../store/orders';
+// import { getAllProductsThunk } from '../../../store/products';
 import CartItemCard from '../CartItemCard';
 import './UserCart.css';
 
 function UserCart() {
     const dispatch = useDispatch();
     const history = useHistory();
-    const cartItems = useSelector(state => state.cart.userCart)
+    const cartItems = useSelector(state => state.cart.userCart);
+    // const products = useSelector(state => state.products.allProducts);
 
     const cartItemsArr = Object.values(cartItems);
 
     let quantityTotal = 0;
     let subTotal = 0;
+
+    // useEffect(() => {
+    //     dispatch(getAllProductsThunk())
+    // }, [dispatch])
 
     const dispatchAddOrder = (e) => {
         e.preventDefault();
@@ -36,31 +43,33 @@ function UserCart() {
     }, [dispatch])
 
     return (
-        <div className='user-cart-container'>
-            <div className='cart-items-div'>
-                <h2 className='cart-items-header'>Shopping Cart</h2>
-                {Object.values(cartItems).length ? Object.values(cartItems).map((item) => {
-                    return (
-                        <CartItemCard key={`cart-item-${item.id}`} item={item}/>
-                    )
-                }) :
-                <h2>Please add items to your cart to see them here.</h2>
-                }
-                <div className='user-cart-totals'>
-                    <div className='subtotal-price'>
-                        {`Subtotal (${quantityTotal} Items): ${Number(subTotal).toFixed(2)}`}
+        <>
+            <div className='user-cart-container'>
+                <div className='cart-items-div'>
+                    <h2 className='cart-items-header'>Shopping Cart</h2>
+                    {Object.values(cartItems).length ? Object.values(cartItems).map((item) => {
+                        return (
+                            <CartItemCard key={`cart-item-${item.id}`} item={item}/>
+                        )
+                    }) :
+                    <h2>Please add items to your cart to see them here.</h2>
+                    }
+                    <div className='user-cart-totals'>
+                        <div className='subtotal-price'>
+                            {`Subtotal (${quantityTotal} Items): ${Number(subTotal).toFixed(2)}`}
+                        </div>
                     </div>
                 </div>
+                {Object.values(cartItems).length ? <div className='uc-checkout-div'>
+                    <div>Your order qualifies for FREE Shipping.</div>
+                    <div className='subtotal-price-checkout'>
+                        {`Subtotal (${quantityTotal} Items): ${Number(subTotal).toFixed(2)}`}
+                    </div>
+                    <button className='uc-order-btn' onClick={dispatchAddOrder}>Checkout</button>
+                </div> :
+                <div className='uc-checkout-div'>Please add items to your cart in order to proceed to checkout.</div>}
             </div>
-            {Object.values(cartItems).length ? <div className='uc-checkout-div'>
-                <div>Your order qualifies for FREE Shipping.</div>
-                <div className='subtotal-price-checkout'>
-                    {`Subtotal (${quantityTotal} Items): ${Number(subTotal).toFixed(2)}`}
-                </div>
-                <button className='uc-order-btn' onClick={dispatchAddOrder}>Checkout</button>
-            </div> :
-            <div className='uc-checkout-div'>Please add items to your cart in order to proceed to checkout.</div>}
-        </div>
+        </>
     )
 }
 
