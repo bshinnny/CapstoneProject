72c123d19c0a954871054e8d6d90fcf5206d6c39
Brandon Shin <brandonjshin98@gmail.com>
2023-02-01 15:16:13 -0800

    Component basics updated.

--- react-app/src/components/Orders/UserOrderCard/index.js
+++ react-app/src/components/Orders/UserOrderCard/index.js
@@ -1,3 +1,15 @@
 import { NavLink } from 'react-router-dom';
 import ErrorImg from '../../../images/unhappy.jpeg'
 import './UserOrderCard.css'
+
+function UserOrderCard({ order }) {
+    return (
+        <div className='order-card-cont'>
+            <NavLink className='order-link' to={`/orders/current/${order.id}`}>
+
+            </NavLink>
+        </div>
+    )
+}
+
+export default UserOrderCard;
--- react-app/src/components/Orders/UserOrders/index.js
+++ react-app/src/components/Orders/UserOrders/index.js
@@ -3,3 +3,24 @@ import { useDispatch, useSelector } from 'react-redux';
 import * as orderActions from '../../../store/orders';
 import UserOrderCard from '../UserOrderCard';
 import './UserOrderCard.css';
+
+function UserOrders() {
+    const dispatch = useDispatch();
+    const orders = useSelector(state.orders.userOrders);
+
+    useEffect(() => {
+        dispatch(orderActions.getUserOrdersThunk())
+    }, [dispatch])
+
+    return (
+        <div className='user-orders-cont'>
+            <div className='user-orders-card-div'>
+                {Object.values(orders).map((order) => {
+                    return (
+                        <UserOrderCard key={`order-${order.id}`} order={order}/>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
